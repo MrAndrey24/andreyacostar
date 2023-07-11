1be1c569f942928f62a7350b7133f9a98b0317bc
Andrey Acosta  R <andreyacostar@gmail.com>
2023-07-11 17:26:17 -0600

    Update cv.js

--- cv.js
+++ cv.js
@@ -1,18 +1,13 @@
-// Obtén la referencia al botón de descarga
 const downloadBtn = document.getElementById("download-btn");
 
-// Define los enlaces de los dos CVs
 const cv1Link = "cv/CV-Andrey_Acosta_Ramirez.pdf";
 const cv2Link = "cv/CV-Andrey_Acosta_Ramirez.pdf";
 
-// Agrega un evento de clic al botón de descarga
 downloadBtn.addEventListener("click", function () {
-  // Descarga los dos CVs
   downloadCV(cv1Link);
   downloadCV(cv2Link);
 });
 
-// Función para descargar un archivo dado su enlace
 function downloadCV(link) {
   const anchor = document.createElement("a");
   anchor.href = link;
