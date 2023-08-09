3ddc3f92567f68e06e6f8779455987e0c7d9d31f
Andrey Acosta  R <andreyacostar@gmail.com>
2023-08-09 16:12:04 -0600

    refactoring email.js

--- js/email.js
+++ js/email.js
@@ -8,45 +8,66 @@ const templateID = "template_4gxnm89";
 
 emailjs.init("D87yrRIrFFult1XnG");
 
-window.onload = function () {
-  // Validate if elements exist
-  if (!btnSendEmail || !user_name || !user_email || !user_message) {
-    Swal.fire({
-      icon: "error",
-      title: "One or more elements are missing. Make sure IDs are correct.",
+function showError(title, text) {
+  Swal.fire({
+    icon: "error",
+    title: title,
+    text: text,
+  });
+}
+
+function showSuccess(title, text) {
+  Swal.fire({
+    icon: "success",
+    title: title,
+    text: text,
+  });
+}
+
+function validateForm() {
+  if (!user_name.value || !user_email.value || !user_message.value) {
+    showError("One or more fields are empty.", "Check the form and try again.");
+    return false;
+  }
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(user_email.value)) {
+    showError("Invalid email format.", "Please enter a valid email address.");
+    return false;
+  }
+
+  return true;
+}
+
+function sendEmail() {
+  if (!validateForm()) {
+    return;
+  }
+
+  emailjs
+    .sendForm(serviceID, templateID, document.getElementById("contact-form"))
+    .then(function () {
+      showSuccess(
+        "Email sent successfully",
+        "I will contact you as soon as possible"
+      );
+    })
+    .catch(function () {
+      showError("Oops...", "Something went wrong!");
     });
+}
+
+window.onload = function () {
+  if (!btnSendEmail || !user_name || !user_email || !user_message) {
+    showError(
+      "One or more elements are missing.",
+      "Make sure IDs are correct."
+    );
     return;
   }
 
   btnSendEmail.addEventListener("click", function (event) {
     event.preventDefault();
-
-    // Validate form
-    if (!user_name.value || !user_email.value || !user_message.value) {
-      Swal.fire({
-        icon: "error",
-        title: "One or more fields are empty.",
-        text: "Check the form and try again.",
-      });
-      return;
-    }
-
-    // Continue with sending email
-    emailjs
-      .sendForm(serviceID, templateID, document.getElementById("contact-form"))
-      .then(function () {
-        Swal.fire({
-          icon: "success",
-          title: "Email sent successfully",
-          text: "I will contact you as soon as possible",
-        });
-      })
-      .catch(function () {
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "Something went wrong!",
-        });
-      });
+    sendEmail();
   });
 };
