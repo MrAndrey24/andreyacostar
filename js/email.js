71b12f34432f3760c1356f27c16779779bbac1fa
Andrey Acosta  R <andreyacostar@gmail.com>
2023-08-09 14:55:05 -0600

    :100:

---
+++ js/email.js
@@ -0,0 +1,8 @@
+const btnSendEmail = document.getElementById("send-email");
+const user_name = document.getElementById("user_name");
+const user_email = document.getElementById("user_email");
+const user_mesaage = document.getElementById("message");
+
+const serviceID = "service_jr6kf7z";
+const templateID = "template_4gxnm89";
+emailjs.init("D87yrRIrFFult1XnG");
